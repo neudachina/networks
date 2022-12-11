da1f925aca29afc635ad464c1589dfdfa1fe1b8b
eva <61154386+neudachina@users.noreply.github.com>
2022-12-11 22:30:53 +0600

    add wget script

---
+++ wget.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /opt/unetlab/addons/iol/bin/
+wget http://dl.nextadmin.net/dl/EVE-NG-image/iol/bin/i86bi_LinuxL2-AdvEnterpriseK9-M_152_May_2018.bin
+wget http://dl.nextadmin.net/dl/EVE-NG-image/iol/bin/i86bi_LinuxL3-AdvEnterpriseK9-M2_157_3_May_2018.bin
